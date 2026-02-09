f4371e6175a1632789f55d0c1b28b7ebbd0ab04a
Omkar Sutar <omkarsutar37@gmail.com>
2026-02-09 15:19:30 +0530

    after cart and products page public

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,14 +40,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1289d735098de847ff733c07c3633f0b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e5fe6273d0b23ce948b7cea70acefdf0",
+"flutter_bootstrap.js": "8ad92b07a4d698c62e4f47fb1cdd6ea2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "fa394b12cf0431d0b57abe0f01feff2a",
 "/": "fa394b12cf0431d0b57abe0f01feff2a",
-"main.dart.js": "23bfabd7c6f4cfcd20cea9d9add3de6f",
+"main.dart.js": "b1b97b2a5590ecab8d9737a6b0021143",
 "manifest.json": "07eabd3fcc1a9f3a6f1a96fdd0fb32b0",
 "version.json": "6279b240ff3d46770d2459f7399c12ce"};
 // The application shell files that are downloaded before a service worker can
